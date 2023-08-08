975b47e8e3a33147063a7a05baf10fc7b5454e7a
Kybersutr <kybersutr@gmail.com>
2023-08-08 11:14:32 -0400

    Add Game Board example

---
+++ AaduPuliAattam/Graph.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaduPuliAattam
+{
+    internal class Vertex
+    {
+        private (int, int) Position { get; set; }
+        public Vertex(int x, int y) 
+        {
+            this.Position = (x, y);
+        }
+    }
+    internal class Graph
+    {
+        private Vertex[] vertices;
+        private (Vertex, Vertex)[] edges;
+    }
+}
