153a887726e4211200dceac590d60fa45794cafc
Kybersutr <kybersutr@gmail.com>
2023-09-14 21:46:24 +0200

    auto set lambs

--- AaduPuliAattam/Menu.cs
+++ AaduPuliAattam/Menu.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace AaduPuliAattam
+﻿namespace AaduPuliAattam
 {
     public partial class Menu : Form
     {
@@ -47,6 +36,12 @@ namespace AaduPuliAattam
             treshold = recommendedTreshold;
 
             mode = 0;
+
+            numericUpDown1.Maximum = maxLambs;
+            numericUpDown1.Value = recommendedLambs;
+
+            numericUpDown2.Value = recommendedTreshold;
+            numericUpDown2.Maximum = lambs;
         }
 
 
@@ -75,6 +70,18 @@ namespace AaduPuliAattam
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             boardPath = Path.Combine(dir, "GameBoards", "Simple.brd");
+            maxLambs = 7;
+            recommendedLambs = 5;
+            lambs = recommendedLambs;
+
+            recommendedTreshold = 2;
+            treshold = recommendedTreshold;
+
+            numericUpDown1.Maximum = maxLambs;
+            numericUpDown1.Value = recommendedLambs;
+
+            numericUpDown2.Value = recommendedTreshold;
+            numericUpDown2.Maximum = lambs;
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
@@ -97,6 +104,18 @@ namespace AaduPuliAattam
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
             boardPath = Path.Combine(dir, "GameBoards", "Advanced.brd");
+            maxLambs = 26;
+            recommendedLambs = 21;
+            lambs = recommendedLambs;
+
+            recommendedTreshold = 9;
+            treshold = recommendedTreshold;
+
+            numericUpDown1.Maximum = maxLambs;
+            numericUpDown1.Value = recommendedLambs;
+
+            numericUpDown2.Value = recommendedTreshold;
+            numericUpDown2.Maximum = lambs;
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
